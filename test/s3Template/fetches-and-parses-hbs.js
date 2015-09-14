6f67b3b5a5f7635e397d650eb14c468bdf5193b4
Andres Serrano <andres.srg@gmail.com>
2015-09-14 18:01:45 -0500

    Fetch and compile HBS templates. Test html output

---
+++ test/s3Template/fetches-and-parses-hbs.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import fs from 'fs';
+// import nock from 'nock';
+import cheerio from 'cheerio';
+
+import { getSpyableCompiler } from '../testUtils';
+// import { take2ApiHost, take2PublicKey } from '../../lib/async-compiler/yamlContext/adapters/take2-adapter';
+
+
+
+const baseDir = './test/s3Template/fixtures';
+
+
+
+function getCompilerWithFixture(fixtureName) {
+  let compiler = getSpyableCompiler();
+    
+  let baseHBS = fs.readFileSync(baseDir + `/${fixtureName}`, 'utf8');
+  compiler.NEXT_S3_RESPONSE = {
+    Body: baseHBS
+  };
+
+  return compiler;
+}
+
+
+describe.only('S3 template', () => {
+
+  it('fetches home, compiles it and adds the right context', () => {
+    let compiler = getCompilerWithFixture('base.hbs');
+
+    return compiler.s3Template.fetchTemplateFor('home')
+      .then(template => {
+        expect(compiler.LAST_FETCH_FROM_S3.key).to.be.equal('home.hbs');
+
+        const context = {
+          title: 'My Page',
+          'featured-banners': {
+            response: {
+              description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione earum culpa iste nihil cupiditate soluta dolorem quaerat, quis unde inventore accusamus, repellat neque tempore praesentium similique reiciendis velit, facere dolor.'
+            }
+          }
+        };
+        const renderedPage = template(context);
+        const $ = cheerio.load(renderedPage);
+        
+        expect($('title').html()).to.be.equal(context.title);
+        expect($('.lorem').html().trim()).to.be.equal(context['featured-banners'].response.description);
+      });
+  });
+
+});
